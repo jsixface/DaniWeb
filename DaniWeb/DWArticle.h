4daba564ad7249937222454d97d89e2763d59209
Arumugam Jeganathan <sixface@Arumugams-MacBook-Pro.local>
2014-07-06 15:24:25 -0400

    Starting to include Restkit in the project

---
+++ DaniWeb/DWArticle.h
@@ -0,0 +1,13 @@
+//
+//  DWArticle.h
+//  DaniWeb
+//
+//  Created by Arumugam Jeganathan on 7/6/14.
+//  Copyright (c) 2014 DaniWeb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DWArticle : NSObject
+
+@end
---
+++ DaniWeb/DWArticle.m
@@ -0,0 +1,13 @@
+//
+//  DWArticle.m
+//  DaniWeb
+//
+//  Created by Arumugam Jeganathan on 7/6/14.
+//  Copyright (c) 2014 DaniWeb. All rights reserved.
+//
+
+#import "DWArticle.h"
+
+@implementation DWArticle
+
+@end
